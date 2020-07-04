1627a985211ae930d2ee4889d52c6c6f1bd51e2c
Ezequiel Gonçalves <ezekiel_sci@hotmail.com>
2020-07-04 12:41:41 -0300

    Changes until JWT logic

---
+++ src/config/auth.ts
@@ -0,0 +1,8 @@
+const authConfig = {
+  jwt: {
+    secret: 'c510155dd9719712b294ba84daed432d',
+    expiresIn: '1d',
+  },
+};
+
+export default authConfig;
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+// eslint-disable-next-line no-console
+app.listen(3333, () => console.log('Server started on port 3333'));
