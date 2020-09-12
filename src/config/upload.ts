e5be860973cfc93b5579113248aded7b8e8dac01
Ezequiel Gonçalves <ezekiel_sci@hotmail.com>
2020-09-11 23:44:43 -0300

    Configuring external services

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,46 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 const uploadsFolder = path.resolve(tempFolder, 'uploads');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder,
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber',
     },
-  }),
-};
+  },
+} as IUploadConfig;
